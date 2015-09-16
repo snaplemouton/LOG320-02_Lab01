89028f9c23419b03be72c204be87506f6d3191d2
snaplemouton <maxime.caron.cem@gmail.com>
2015-09-16 11:29:39 -0400

    Yolo change
    :dancer:

---
+++ Test/Test/Classes/FileManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace Test.Classes
+{
+    public class FileManager
+    {
+        public void CompressFile(FileStream file)
+        {
+            byte[] lstByte = new byte[file.Length];
+            if(file.CanRead)
+            {
+                file.Read(lstByte, 0, (int)file.Length);
+            }
+        }
+
+        public void DecompressFile(FileStream file)
+        {
+
+        }
+    }
+}
